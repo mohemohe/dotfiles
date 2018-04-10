94a6f92e20a2f2650bf3b2a420dd8db7805b9c26
mohemohe <mohemohe@ghippos.net>
2018-04-10 12:05:54 +0900

    hi

--- install.sh
+++ install.sh
@@ -14,14 +14,16 @@ has_curl="$?"
 command -v wget &> /dev/null
 has_wget="$?"
 
-pacapt="$HOME/.local/bin"
-if [[ "$has_curl" == "0" ]]; then
-    curl https://github.com/icy/pacapt/raw/ng/pacapt > "$pacapt"
-elif [[ "$has_wget" == "0" ]]; then
-    wget -O - https://github.com/icy/pacapt/raw/ng/pacapt > "$pacapt"
-else
-    echo curlかwgetをインストールして再実行してください
-    exit 1
+pacapt="$HOME/.local/bin/pacapt"
+if [[ ! -x "$pacapt" ]]; then
+    if [[ "$has_curl" == "0" ]]; then
+        curl https://github.com/icy/pacapt/raw/ng/pacapt > "$pacapt"
+    elif [[ "$has_wget" == "0" ]]; then
+        wget -O - https://github.com/icy/pacapt/raw/ng/pacapt > "$pacapt"
+    else
+        echo curlかwgetをインストールして再実行してください
+        exit 1
+    fi
 fi
 
 chmod +x "$pacapt"
@@ -30,8 +32,8 @@ echo "##################################################"
 echo "  zsh"
 echo "##################################################"
 
-ln -s "$DOTFILES/zsh/" "$HOME/.zsh"
-ln -s "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
+ln -nfs "$DOTFILES/zsh/" "$HOME/.zsh"
+ln -nfs "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
 touch "$HOME/.zshrc2"
 
 echo "##################################################"
@@ -39,6 +41,6 @@ echo "  prezto"
 echo "##################################################"
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/.zprezto"
-ln -s "$HOME/.zprezto/runcoms/zpreztorc" "$HOME/.zpreztorc"
-ln -s "$HOME/.zprezto/runcoms/zprofile" "$HOME/.zprofile"
-ln -s "$HOME/.zprezto/runcoms/zshenv" "$HOME/.zshenv"
+ln -nfs "$HOME/.zprezto/runcoms/zpreztorc" "$HOME/.zpreztorc"
+ln -nfs "$HOME/.zprezto/runcoms/zprofile" "$HOME/.zprofile"
+ln -nfs "$HOME/.zprezto/runcoms/zshenv" "$HOME/.zshenv"
