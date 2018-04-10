752a9edf69facf3e71c778798ad427d679bed2b4
mohemohe <mohemohe@ghippos.net>
2018-04-10 12:40:32 +0900

    hi

--- zsh/detect_terminal.zsh
+++ zsh/detect_terminal.zsh
@@ -6,6 +6,6 @@ if [[ "${TERM_PROGRAM}" != "" ]]; then {
   __TERMINAL_EMULATOR="${TERM_PROGRAM}"
 } else {
   # Linux
-  __TERMINAL_EMULATOR=`basename "/"$(ps -f -p $(cat /proc/$(echo $$)/stat | cut -d \  -f 4) | tail -1 | sed 's/^.* //')` && Z_VERBOSE_LOG DONE "detect terminal emulator: ${__TERMINAL_EMULATOR=}"
+  __TERMINAL_EMULATOR=`basename "/"$(ps -f -p $(cat /proc/$(echo $$)/stat | cut -d \  -f 4) 2> /dev/null | tail -1 | sed 's/^.* //')` && Z_VERBOSE_LOG DONE "detect terminal emulator: ${__TERMINAL_EMULATOR=}"
 } fi
 Z_VERBOSE_LOG DONE "detect terminal ${__TERMINAL_EMULATOR}"
