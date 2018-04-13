63811a36d6a21447ac635fe23ab2449978de8552
もっへもへ <mohemohe@users.noreply.github.com>
2018-04-13 16:36:11 +0900

    えへへ〜〜〜〜〜〜〜〜〜〜〜〜

--- install.sh
+++ install.sh
@@ -17,17 +17,20 @@ function git_pull_or_clone() {
 
 git_pull_or_clone https://github.com/mohemohe/dotfiles.git "$DOTFILES"
 
+set +e
 command -v curl &> /dev/null
 has_curl="$?"
 command -v wget &> /dev/null
 has_wget="$?"
+set -e
 
 pacapt="$HOME/.local/bin/pacapt"
+fetch=
 if [[ ! -x "$pacapt" ]]; then
     if [[ "$has_curl" == "0" ]]; then
-        alias fetch="curl"
+        fetch="curl"
     elif [[ "$has_wget" == "0" ]]; then
-        alias fetch="wget -O -"
+        fetch="wget -O -"
     else
         echo curlかwgetをインストールして再実行してください
         exit 1
@@ -40,7 +43,7 @@ mkdir -p "$HOME/.local/bin"
 if [[ -e "$pacapt" ]]; then
     rm -f "$pacapt"
 fi
-fetch https://github.com/icy/pacapt/raw/ng/pacapt > "$pacapt"
+$fetch https://github.com/icy/pacapt/raw/ng/pacapt > "$pacapt"
 chmod +x "$pacapt"
 
 ## sshconf
@@ -48,7 +51,7 @@ sshconf="$HOME/.local/bin/sshconf"
 if [[ -e "$sshconf" ]]; then
     rm -f "$sshconf"
 fi
-fetch https://raw.githubusercontent.com/mohemohe/sshconf/master/sshconf >| "$sshconf"
+$fetch https://raw.githubusercontent.com/mohemohe/sshconf/master/sshconf >| "$sshconf"
 chmod +x "$sshconf"
 
 ## zsh
