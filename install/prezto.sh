178b23321e83dc4dae5fc22dd036fccf7992d678
もっへもへ <mohemohe@ghippos.net>
2018-04-30 11:23:27 +0900

    preztoのモジュールがデフォルト有効になっていのはクソすぎる

--- install/prezto.sh
+++ install/prezto.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 git_pull_or_clone https://github.com/sorin-ionescu/prezto.git "$HOME/.zprezto"
-ln -nfs "$HOME/.zprezto/runcoms/zpreztorc" "$HOME/.zpreztorc"
 ln -nfs "$HOME/.zprezto/runcoms/zprofile" "$HOME/.zprofile"
 ln -nfs "$HOME/.zprezto/runcoms/zshenv" "$HOME/.zshenv"
--- install/zsh.sh
+++ install/zsh.sh
@@ -2,4 +2,5 @@
 
 ln -nfs "$DOTFILES/zsh" "$HOME/.zsh"
 ln -nfs "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
+ln -nfs "$DOTFILES/zsh/.zpreztorc" "$HOME/.zpreztorc"
 touch "$HOME/.zshrc2"
