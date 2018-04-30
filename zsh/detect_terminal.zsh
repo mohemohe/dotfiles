7c51526651984f27b49d253f804225aee21d18fe
もっへもへ <mohemohe@ghippos.net>
2018-04-30 10:17:06 +0900

    ターミナルエミュレーターの検出ログが2回出るのを修正

--- zsh/detect_terminal.zsh
+++ zsh/detect_terminal.zsh
@@ -6,6 +6,6 @@ if [[ "${TERM_PROGRAM}" != "" ]]; then {
   __TERMINAL_EMULATOR="${TERM_PROGRAM}"
 } else {
   # Linux
-  __TERMINAL_EMULATOR=`basename "/"$(ps -f -p $(cat /proc/$(echo $$)/stat | cut -d \  -f 4) 2> /dev/null | tail -1 | sed 's/^.* //')` && Z_VERBOSE_LOG DONE "detect terminal emulator: ${__TERMINAL_EMULATOR=}"
+  __TERMINAL_EMULATOR=`basename "/"$(ps -f -p $(cat /proc/$(echo $$)/stat | cut -d \  -f 4) 2> /dev/null | tail -1 | sed 's/^.* //')`
 } fi
-Z_VERBOSE_LOG DONE "detect terminal ${__TERMINAL_EMULATOR}"
+Z_VERBOSE_LOG DONE "detect terminal emulator: ${__TERMINAL_EMULATOR}"
