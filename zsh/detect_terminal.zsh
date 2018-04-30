bfe995e2030e590209a4c7170171ca1fef4c761c
もっへもへ <mohemohe@ghippos.net>
2018-04-30 10:50:31 +0900

    ターミナルエミュレーターの検出をマシにした neofetchよりは確実ではないから若干注意

--- zsh/detect_terminal.zsh
+++ zsh/detect_terminal.zsh
@@ -6,6 +6,6 @@ if [[ "${TERM_PROGRAM}" != "" ]]; then {
   __TERMINAL_EMULATOR="${TERM_PROGRAM}"
 } else {
   # Linux
-  __TERMINAL_EMULATOR=`basename "/"$(ps -f -p $(cat /proc/$(echo $$)/stat | cut -d \  -f 4) 2> /dev/null | tail -1 | sed 's/^.* //')`
+  __TERMINAL_EMULATOR=$(ps -o comm= -p "$(($(ps -o ppid= -p "$(($(ps -o sid= -p "$$")))")))")
 } fi
 Z_VERBOSE_LOG DONE "detect terminal emulator: ${__TERMINAL_EMULATOR}"
