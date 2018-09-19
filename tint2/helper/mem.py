c24e4a3a277b760b6707b8bcd936e0c6d658bb96
mohemohe <mohemohe@ghippos.net>
2018-09-19 13:52:52 +0900

    スペース足りない

--- tint2/helper/mem.py
+++ tint2/helper/mem.py
@@ -10,5 +10,5 @@ while True:
     swapFree = float(meminfo.get('SwapFree').split(' ')[0]) / 1024
     swapUsage = swapTotal - swapFree
     with open('/dev/shm/mem', mode='w') as f:
-        f.write(' RAM: %5.2f MB / %5.2f MB\nSWAP:%5.2f MB / %5.2f MB' % (memUsage, memTotal, swapUsage, swapTotal))
+        f.write(' RAM: %5.2f MB / %5.2f MB\nSWAP: %5.2f MB / %5.2f MB' % (memUsage, memTotal, swapUsage, swapTotal))
     time.sleep(1)
