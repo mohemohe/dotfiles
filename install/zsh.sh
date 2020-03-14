ed6bbc22927cbe56b6625674ce332bdc9d9e64c5
mohemohe <mohemohe@ghippos.net>
2020-03-14 23:51:20 +0900

    hi

--- install/zsh.sh
+++ install/zsh.sh
@@ -5,7 +5,9 @@ ln -nfs "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
 ln -nfs "$DOTFILES/zsh/.zshenv" "$HOME/.zshenv"
 ln -nfs "$DOTFILES/zsh/.zpreztorc" "$HOME/.zpreztorc"
 ln -nfs "$DOTFILES/zsh/.zshenv" "$HOME/.zshenv"
-if [[ ! -f "$HOME/.zshconfig" ]]; then {
+
+cat "$HOME/.zshconfig" 1> /dev/null 2> /dev/null
+if [[ "$?" != "0" ]]; then {
     cp -f "$DOTFILES/zsh/.zshconfig" "$HOME/.zshconfig"
 } fi
 touch "$HOME/.zshrc2"
