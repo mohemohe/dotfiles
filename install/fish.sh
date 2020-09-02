0350b24524703dc53069eb4e8767f25de702ed24
mohemohe <mohemohe@ghippos.net>
2020-09-03 05:00:50 +0900

    fix u

--- install/fish.sh
+++ install/fish.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+set +u
+
 if [ "$XDG_CONFIG_HOME" -eq "" ]; then
     XDG_CONFIG_HOME="$HOME/.config"
 fi
+
+set -u
 
 ln -nfs "$DOTFILES/fish" "$HOME/.fish"
 ln -nfs "$DOTFILES/fish/config.fish" "$XDG_CONFIG_HOME/fish/config.fish"
