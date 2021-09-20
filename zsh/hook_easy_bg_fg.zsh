5cc58c0929fcaac7ce592bd872c52fdfad92be37
mohemohe <mohemohe@ghippos.net>
2021-09-20 13:13:18 +0900

    fg bgをかんたんにするやつを追加

---
+++ zsh/hook_easy_bg_fg.zsh
@@ -0,0 +1,11 @@
+easy_bg_fg () {
+    if [[ $#BUFFER -eq 0 ]]; then
+        BUFFER="fg"
+        zle accept-line
+    else
+        zle push-input
+        zle clear-screen
+    fi
+}
+zle -N easy_bg_fg
+bindkey '^z' easy_bg_fg
