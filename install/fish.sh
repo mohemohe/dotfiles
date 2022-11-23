e62cb88fc32d6eb2452c4eeff8119ebd21eb1acc
mohemohe <mohemohe@ghippos.net>
2022-11-23 21:01:52 +0900

    reset fish dir

--- install/fish.sh
+++ install/fish.sh
@@ -9,6 +9,7 @@ fi
 set -u
 
 rm -rf "$XDG_CONFIG_HOME/fish"
+mkdir "$XDG_CONFIG_HOME/fish"
 
 ln -nfs "$DOTFILES/fish" "$HOME/.fish"
 ln -nfs "$DOTFILES/fish/config.fish" "$XDG_CONFIG_HOME/fish/config.fish"
