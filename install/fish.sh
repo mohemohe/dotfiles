efe843c38d0eb647e20384742eaf228e0f807f93
mohemohe <mohemohe@ghippos.net>
2022-11-23 20:56:34 +0900

    fisher

--- install/fish.sh
+++ install/fish.sh
@@ -10,6 +10,5 @@ set -u
 
 ln -nfs "$DOTFILES/fish" "$HOME/.fish"
 ln -nfs "$DOTFILES/fish/config.fish" "$XDG_CONFIG_HOME/fish/config.fish"
-ln -nfs "$DOTFILES/fish/fish_plugins" "$XDG_CONFIG_HOME/fish/fish_plugins"
 
 touch "$HOME/.fish/user.fish"
--- install/fisher.sh
+++ install/fisher.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+ln -nfs "$DOTFILES/fish/fish_plugins" "$XDG_CONFIG_HOME/fish/fish_plugins"
 
 /usr/bin/env fish -c "fisher update"
