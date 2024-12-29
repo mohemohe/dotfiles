70dbb681c2fee40f7f62340c66d800a0e1674386
mohemohe <mohemohe@ghippos.net>
2024-12-30 01:01:18 +0900

    a

--- fish/include/60.goenv.fish
+++ fish/include/60.goenv.fish
@@ -3,6 +3,7 @@ export GOENV_ROOT="$HOME/.goenv"
 if [ -d "$GOENV_ROOT" ]
   if not contains -- "$GOENV_ROOT/bin" $fish_user_paths
     set -U fish_user_paths "$GOENV_ROOT/bin" $fish_user_paths
+        set -U fish_user_paths "$GOENV_ROOT/shims" $fish_user_paths
   end
   goenv init - | source
   Z_VERBOSE_LOG DONE 'initialize goenv'
