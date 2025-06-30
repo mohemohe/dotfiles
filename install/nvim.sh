224b62f1cf3850ef2dbced3f8a8fe6e4ae693e3f
mohemohe <mohemohe@ghippos.net>
2025-07-01 00:55:19 +0900

    feat: Replace NeoVim setup with LazyVim

--- install/nvim.sh
+++ install/nvim.sh
@@ -1,14 +1,38 @@
 #!/bin/sh
 
-mkdir -p "${HOME}/.local/share/nvim/site/autoload"
-mkdir -p "${HOME}/.config/nvim"
-cp -rf "${DOTFILES}/vim/.vim" "${HOME}/"
-ln -nfs "${DOTFILES}/vim/.vimrc" "${HOME}/.config/nvim/init.vim"
-touch "${HOME}/.vimrc2"
-
-rm -f "${HOME}/.local/share/nvim/site/autoload/plug.vim"
-fetch https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > "${HOME}/.local/share/nvim/site/autoload/plug.vim"
-
-if [ "${CI}" != "1" ]; then
-    nvim +silent +VimEnter +PlugInstall +qall
+# Backup current Neovim files if they exist
+if [ -d "${HOME}/.config/nvim" ] && [ ! -L "${HOME}/.config/nvim" ]; then
+    echo "Backing up current Neovim configuration..."
+    mv "${HOME}/.config/nvim" "${HOME}/.config/nvim.bak.$(date +%Y%m%d_%H%M%S)"
 fi
+
+if [ -d "${HOME}/.local/share/nvim" ]; then
+    echo "Backing up Neovim data..."
+    mv "${HOME}/.local/share/nvim" "${HOME}/.local/share/nvim.bak.$(date +%Y%m%d_%H%M%S)"
+fi
+
+if [ -d "${HOME}/.local/state/nvim" ]; then
+    echo "Backing up Neovim state..."
+    mv "${HOME}/.local/state/nvim" "${HOME}/.local/state/nvim.bak.$(date +%Y%m%d_%H%M%S)"
+fi
+
+if [ -d "${HOME}/.cache/nvim" ]; then
+    echo "Backing up Neovim cache..."
+    mv "${HOME}/.cache/nvim" "${HOME}/.cache/nvim.bak.$(date +%Y%m%d_%H%M%S)"
+fi
+
+# Remove existing symlink if it exists
+if [ -L "${HOME}/.config/nvim" ]; then
+    rm -f "${HOME}/.config/nvim"
+fi
+
+# Clone LazyVim starter
+echo "Installing LazyVim..."
+git clone https://github.com/LazyVim/starter "${HOME}/.config/nvim"
+
+# Remove .git folder
+rm -rf "${HOME}/.config/nvim/.git"
+
+echo "LazyVim installation complete!"
+echo "Run 'nvim' to start Neovim with LazyVim"
+echo "It is recommended to run ':LazyHealth' after first launch"
